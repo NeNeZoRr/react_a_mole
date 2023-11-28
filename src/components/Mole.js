adeba7553925511140a742c0a3d6904bbde1b265
NeNeZoRr <zorrillalin@gmail.com>
2023-11-28 14:05:56 -0500

    Creation of MoleContainer

--- src/components/Mole.js
+++ src/components/Mole.js
@@ -2,27 +2,27 @@ import { useEffect } from 'react';
 import moleImg from '../mole.png';
 
 function Mole(props) {
-  useEffect(() => {
-    const baseTimeout = 2000; 
-    const randomFactor = Math.random() * 1000;
+    useEffect(() => {
+        const baseTimeout = 2000;
+        const randomFactor = Math.random() * 1000;
 
-    const timer = setTimeout(() => {
-      props.setDisplayMole(false);
-    }, baseTimeout + randomFactor);
+        const timer = setTimeout(() => {
+            props.setDisplayMole(false);
+        }, baseTimeout + randomFactor);
 
-    return () => clearTimeout(timer);
-  }, [props]);
+        return () => clearTimeout(timer);
+    }, [props]);
 
-  return (
-    <div>
-      <img
-        style={{ width: '30vw' }}
-        src={moleImg}
-        alt="Mole"
-        onClick={props.handleClick}
-      />
-    </div>
-  );
+    return (
+        <div>
+            <img
+                style={{ width: '30vw' }}
+                src={moleImg}
+                alt="Mole"
+                onClick={props.handleClick}
+            />
+        </div>
+    );
 }
 
-export default Mole;
+export default Mole
---
+++ src/components/MoleContainer.js
@@ -0,0 +1,27 @@
+
+import React, { useState } from 'react';
+import Mole from './Mole';
+import EmptySlot from './EmptySlot';
+
+function MoleContainer({ score, setScore }) {
+    const [displayMole, setDisplayMole] = useState(false);
+
+    const handleMoleClick = () => {
+        setScore(prevScore => prevScore + 1);
+        setDisplayMole(false);
+    };
+
+    const moleOrEmptySlot = displayMole ? (
+        <Mole
+            setScore={setScore}
+            score={score}
+            handleClick={handleMoleClick}
+        />
+    ) : (
+        <EmptySlot />
+    );
+
+    return <div className="mole-container">{moleOrEmptySlot}</div>;
+}
+
+export default MoleContainer;
